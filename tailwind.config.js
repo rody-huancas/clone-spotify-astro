6b8bbc16b33d1353f276fc8f8cd785003f850333
Rody Huancas <rodyhuancas.04@gmail.com>
2024-05-28 14:06:40 -0500

    Add: configuration initial

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,jsx,mdx,svelte,ts,tsx,vue,astro}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
