b322c8b9d5cf27ca8c9057974849c329a1b43afe
Rody Huancas <rodyhuancas.04@gmail.com>
2024-06-02 21:59:23 -0500

    Fix: classname icon card play button

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -22,14 +22,14 @@ export const CardPlayButton = ({ id, size = "small" }) => {
       });
   };
 
-  const iconClassName = size === "smal" ? "w-4 h-4" : "w-5 h-5";
+  const iconClassName = size === "small" ? "w-4 h-4" : "w-5 h-5";
 
   return (
     <button
       onClick={handleClick}
       className="card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400"
     >
-      {isPlayingPlaylist ? <Pause className="iconClassName" /> : <Play className="iconClassName" />}
+      {isPlayingPlaylist ? <Pause className={iconClassName} /> : <Play className={iconClassName} />}
     </button>
   );
 };
